bb288292268ec0ce101da8eaf60ede7d23e26fed
Prashant Gaur <prashantgaur.in@gmail.com>
2023-05-26 16:17:52 +0000

    Fix configuration for rtd theme with extensions config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,12 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme',]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
